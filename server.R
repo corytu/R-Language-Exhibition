26e821a4c76be85ea1208d4574ac8d129755683d
Yu-Zhen Tu <corytu@users.noreply.github.com>
2017-02-18 23:49:24 +0800

    Add files via upload
    A Shiny App featuring plots with the built-in data frame, "airquality".

--- server.R
+++ server.R
@@ -3,33 +3,34 @@ library(ggplot2)
 
 # Define server logic required to draw a line chart
 shinyServer(function(input, output) {
-  # month_selected <- reactive({which(input$month == month.name)})
-  my_air <- airquality
-  my_air$Month <- sapply(1:nrow(my_air), function(i) {
-    month.name[my_air$Month[i]]
-  })
+  month_selected <- reactive({match(input$month, month.name)})
+  # my_air <- airquality
+  # my_air$Month <- sapply(1:nrow(my_air), function(i) {
+  #   month.name[my_air$Month[i]]
+  # })
   output$airplot <- renderPlot({
     if (input$obs == "Ozone") {
       ggplot(aes(x = Day, y = Ozone),
-             data = subset(my_air, Month == input$month)) +
+             data = subset(airquality, Month == month_selected())) +
+        # Note that is month_selected() rather than month_selected
         geom_line(size = 1) + geom_smooth() +
         labs(list(x = "Day of Month", y = "Mean Ozone (ppb)"))
     }
     else if (input$obs == "Solar.R") {
       ggplot(aes(x = Day, y = Solar.R),
-             data = subset(my_air, Month == input$month)) +
+             data = subset(airquality, Month == month_selected())) +
         geom_line(size = 1) + geom_smooth() +
         labs(list(x = "Day of Month", y = "Solar Radiation (Langleys)"))
     }
     else if (input$obs == "Wind") {
       ggplot(aes(x = Day, y = Wind),
-             data = subset(my_air, Month == input$month)) +
+             data = subset(airquality, Month == month_selected())) +
         geom_line(size = 1) + geom_smooth() +
         labs(list(x = "Day of Month", y = "Average Wind Speed (miles/hour)"))
     }
     else {
       ggplot(aes(x = Day, y = Temp),
-             data = subset(my_air, Month == input$month)) +
+             data = subset(airquality, Month == month_selected())) +
         geom_line(size = 1) + geom_smooth() +
         labs(list(x = "Day of Month",
                   y = expression(paste("Maximun Daily Temperature (", degree ~ F, ")", sep = ""))))
