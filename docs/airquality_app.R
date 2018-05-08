583e2a0692ee6b1d47eeff753ce120f1f56a1922
corytu <corytu@users.noreply.github.com>
2018-05-08 23:52:30 +0800

    Refactor airquality_app.R
    Instead of using if-else to handle different selected features, I use aes_string function and create a list for different axis labels, which are what I've learned in recent years.

--- docs/airquality_app.R
+++ docs/airquality_app.R
@@ -35,36 +35,21 @@ ui <- fluidPage(
   )
 )
 
+ylab_list <- list(
+  Ozone = "Mean Ozone (ppb)",
+  Solar.R = "Solar Radiation (Langleys)",
+  Wind = "Average Wind Speed (miles/hour)",
+  Temp = expression(paste("Maximun Daily Temperature (", degree ~ F, ")", sep = ""))
+)
+
 # Define server logic required to draw a line chart
 server <- function(input, output) {
   month_selected <- reactive({match(input$month, month.name)})
   output$airplot <- renderPlot({
-    if (input$obs == "Ozone") {
-      ggplot(aes(x = Day, y = Ozone),
-             data = subset(airquality, Month == month_selected())) +
-        # Note that is month_selected() rather than month_selected
-        geom_line(size = 1) + geom_smooth() +
-        labs(list(x = "Day of Month", y = "Mean Ozone (ppb)"))
-    }
-    else if (input$obs == "Solar.R") {
-      ggplot(aes(x = Day, y = Solar.R),
-             data = subset(airquality, Month == month_selected())) +
-        geom_line(size = 1) + geom_smooth() +
-        labs(list(x = "Day of Month", y = "Solar Radiation (Langleys)"))
-    }
-    else if (input$obs == "Wind") {
-      ggplot(aes(x = Day, y = Wind),
-             data = subset(airquality, Month == month_selected())) +
-        geom_line(size = 1) + geom_smooth() +
-        labs(list(x = "Day of Month", y = "Average Wind Speed (miles/hour)"))
-    }
-    else {
-      ggplot(aes(x = Day, y = Temp),
-             data = subset(airquality, Month == month_selected())) +
-        geom_line(size = 1) + geom_smooth() +
-        labs(list(x = "Day of Month",
-                  y = expression(paste("Maximun Daily Temperature (", degree ~ F, ")", sep = ""))))
-    }
+    ggplot(aes_string(x = "Day", y = input$obs),
+                data = subset(airquality, Month == month_selected())) +
+      geom_line(size = 1) + geom_smooth() +
+      labs(list(x = "Day of Month", y = ylab_list[[input$obs]]))
   })
 }
 
